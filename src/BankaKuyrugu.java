acb686e64ca7d065a83830f6293c5dd158dd8ebc
Burak Emre DOST <burakemredost@gmail.com>
2012-11-12 21:47:54 +0200

    gise
    project has been finished

--- src/BankaKuyrugu.java
+++ src/BankaKuyrugu.java
@@ -13,7 +13,12 @@ public class BankaKuyrugu {
 		 int secim;
 		 
 		 Scanner in=new Scanner(System.in);
-		 
+		 System.out.println("Kuyruða eklenecek eleman sayýsýný giriniz:");
+		 int n=in.nextInt();
+		 System.out.println("\n\n");
+	 	 Kuyruk Banka=new Kuyruk(n);
+	 	 Banka.ekleme();
+	 	 ArrayList<Musteri> gise=(ArrayList<Musteri>)Banka.banka_kuyruk.clone();
 		 
 		 do{
 			 secim=menu();
@@ -21,11 +26,8 @@ public class BankaKuyrugu {
 			 	case 1:
 			 	{
 			 		
-					System.out.println("Kuyruga eklenecek eleman sayisini giriniz:");
-					int n=in.nextInt();
-					System.out.println("\n\n");
-			 		Kuyruk Banka=new Kuyruk(n);
-			 		Banka.ekleme();
+					
+					
 					Banka.yazdir();
 					System.out.println();
 					System.out.println();
@@ -35,7 +37,8 @@ public class BankaKuyrugu {
 			 	}
 			 	case 2:
 			 	{
-			 	    gise_olusturma();
+			 	    gise_olusturma(n,gise);
+			 	    Banka.ortalamalarý_yaz();
 			 		break;
 			 	}
 			 }
@@ -49,54 +52,74 @@ public class BankaKuyrugu {
 	{
 		int secim;
 		
-		System.out.println("1) FIFO kuyrugu ve oncelik kuyruï¿½u oluï¿½turma");
-		System.out.println("2) 3 adet FIFO ve ï¿½ncelik kuyruï¿½u oluï¿½turup zaman kï¿½yaslama");
-		System.out.println("3) ï¿½ï¿½kï¿½ï¿½");
+		System.out.println("1) FIFO kuyruðu ve öncelik kuyruðu oluþturma");
+		System.out.println("2) 3 adet FIFO ve öncelik kuyruðu oluþturup zaman kýyaslama");
+		System.out.println("3) Çýkýþ");
 		System.out.println();
-		System.out.println("Seï¿½iminizi giriniz:");
+		System.out.println("Seçiminizi giriniz:");
 		Scanner in = new Scanner(System.in);
 		secim=in.nextInt();
 		
 		return secim;
 	}
 	
-	public static void gise_olusturma(){
-		Scanner in=new Scanner(System.in);
-		System.out.println("Kuyruï¿½a eklenecek eleman sayï¿½sï¿½nï¿½ giriniz:");
-		int n=in.nextInt();
-		int x=0;
+	public static void gise_olusturma(int n,ArrayList<Musteri> kuyruk){
 		
-		//Random rand= new Random();
-		Kuyruk kuyruk1=new Kuyruk(n,x);
- 		Kuyruk kuyruk2=new Kuyruk(n,x);
- 		Kuyruk kuyruk3=new Kuyruk(n,x);
+		Kuyruk kuyruk1=new Kuyruk(n,kuyruk);
+ 		Kuyruk kuyruk2=new Kuyruk(n,kuyruk);
+ 		Kuyruk kuyruk3=new Kuyruk(n,kuyruk);
  		
- 		//ArrayList<Musteri> temp1=kuyruk1.banka_kuyruk;
- 		//ArrayList<Musteri> temp2=kuyruk2.banka_kuyruk;
- 		//ArrayList<Musteri> temp3=kuyruk3.banka_kuyruk;
+ 		
  		for(int i=0;i<n;i++){
  		
- 			if(kuyruk1.banka_kuyruk.size()<kuyruk2.banka_kuyruk.size() && kuyruk1.banka_kuyruk.size()<kuyruk3.banka_kuyruk.size())
- 				kuyruk1.tekli_ekle(i+1);
+ 			if(kuyruk1.boyut<=kuyruk2.boyut && kuyruk1.boyut<=kuyruk3.boyut)
+ 				kuyruk1.tekli_ekle(i);
  			else{
- 				if(kuyruk2.banka_kuyruk.size()<kuyruk3.banka_kuyruk.size())
- 					kuyruk2.tekli_ekle(i+1);
+ 				if(kuyruk2.boyut<=kuyruk3.boyut)
+ 					kuyruk2.tekli_ekle(i);
  				else{
- 					kuyruk3.tekli_ekle(i+1);
+ 					kuyruk3.tekli_ekle(i);
  				     }
  			     }
  			
  		    
  		}
- 		System.out.println("1. Gï¿½ï¿½E");
- 		kuyruk1.yazdir();
+ 		System.out.println("1. Giþenin bekleme süresi");
+ 		float sure1=kuyruk1.gise_sure();
+ 		System.out.println(sure1+"\n\n");
+ 		System.out.println("2. Giþenin bekleme süresi");
+ 		float sure2=kuyruk2.gise_sure();
+ 		System.out.println(sure2+"\n\n");
+ 		System.out.println("3. Giþenin bekleme süresi");
+ 		float sure3=kuyruk3.gise_sure();
+ 		System.out.println(sure3+"\n\n");
+ 		float genel_fifo_ort=sure1+sure2+sure3;
+ 		
+ 		
+ 		System.out.println("1. Giþenin öncelikli ortalama bekleme süresi");
+ 		
+ 		kuyruk1.gise_sirala();
+ 		kuyruk1.yazdir3();
+ 		
  		System.out.println("\n\n");
- 		System.out.println("2. Gï¿½ï¿½E");
- 		kuyruk2.yazdir();
+ 		System.out.println("2. Giþenin öncelikli ortalama bekleme süresi");
+ 		
+ 		kuyruk2.gise_sirala();
+ 		kuyruk2.yazdir3();
+ 		
  		System.out.println("\n\n");
- 		System.out.println("3. Gï¿½ï¿½E");
- 		kuyruk3.yazdir();
+ 		System.out.println("3.Giþenin öncelikli ortalama bekleme süresi");
+ 		
+ 		kuyruk3.gise_sirala();
+ 		kuyruk3.yazdir3();
  		System.out.println("\n\n");
+ 		
+ 		float gise_ort=kuyruk1.onc_ort()+kuyruk2.onc_ort()+kuyruk3.onc_ort();
+ 		
+ 		System.out.println("Tek giþe ve üç giþe arasýndaki zaman farklarý");
+ 		System.out.println("FIFO toplamlarý:"+genel_fifo_ort);
+ 		System.out.println("Öncelik toplamlarý:"+gise_ort);
+ 		
 	}
 	
 
--- src/Kuyruk.java
+++ src/Kuyruk.java
@@ -4,13 +4,17 @@ public class Kuyruk {
 	
 	public int boyut;
 	public ArrayList<Musteri> banka_kuyruk;
-	
+	public ArrayList<Musteri> gise_kuyruk;
 	public int[] bekleme_suresi;
 	public int[] bekleme_suresi_oncelik;
 	public double ort_sure;
+	public double onc_ort_sure;
+	public float gise_onc_ort;
 	
-	public Kuyruk(int sayi,int l){
+	public Kuyruk(int sayi,ArrayList<Musteri> liste){
 		banka_kuyruk=new ArrayList<Musteri>();
+		gise_kuyruk=new ArrayList<Musteri>();
+		banka_kuyruk=(ArrayList<Musteri>)liste.clone();
         boyut=0;
 	    bekleme_suresi=new int[sayi];
 		bekleme_suresi_oncelik=new int[sayi];
@@ -42,9 +46,12 @@ public class Kuyruk {
 	}
 	
 	public void tekli_ekle(int k){
-		Musteri birMusteri=new Musteri(k);
-		banka_kuyruk.add(birMusteri);
+		Musteri birMusteri=banka_kuyruk.get(k);
+		gise_kuyruk.add(birMusteri);
 		boyut++;
+		
+		
+	
 	}
 	
 	public void yazdir(){
@@ -60,6 +67,7 @@ public class Kuyruk {
 		System.out.println("Ortalama iþlem süresi:"+ort);
 		ort_sure=ort;
 	}
+	
 	
 	public void sirala(){
 		
@@ -103,8 +111,80 @@ public class Kuyruk {
 		double oncelik_ort=(double)toplam_sure/boyut;
 		System.out.println("ortalama iþlem süresindeki fark:"+(ort_sure-oncelik_ort));
 		System.out.println("Kazanç yüzdesi:%"+(double)(ort_sure-oncelik_ort)/ort_sure*100);
+		onc_ort_sure=oncelik_ort;
 	}
+	
+	public float gise_sure(){
+		int beklenen_sure=0;
+		int[] beks=new int[boyut];
+		Musteri temp;
+		
+		for(int i=0;i<gise_kuyruk.size();i++){
+			temp=gise_kuyruk.get(i);
+			beklenen_sure+=temp.islem_suresi;
+			beks[i]=beklenen_sure;
+			}
+		int sure=0;
+		for(int i=0;i<beks.length;i++){
+			
+			sure+=beks[i];
+		}
+		
+		float sure2=(float)sure/boyut;
+		return sure2;
+	}
+	
+public void gise_sirala(){
+		
+		
+		Musteri temp1;
+		Musteri temp2;
+		
+		
+		
+		for (int i = 1; i < boyut; i++) {
+		     for (int j = 0; j < boyut - i; j++)
+		    {
+		    	 temp1=gise_kuyruk.get(j);
+		    	  temp2=gise_kuyruk.get(j+1);
+		         if(temp1.islem_suresi>temp2.islem_suresi)
+		        {
+		             Collections.swap(gise_kuyruk, j, j+1);
+		         }
+		     }
+		     
+		}
+	}
+	
+	public void yazdir3(){
+		int sure=0;
+		int toplam_sure=0;
+		
+		for(int i=0;i<boyut;i++){
+			Musteri temp=gise_kuyruk.get(i);
+			sure+=temp.islem_suresi;
+			bekleme_suresi_oncelik[temp.sira-1]=sure;
+			if(bekleme_suresi_oncelik[temp.sira-1]<bekleme_suresi[temp.sira-1]){
+				System.out.println("FIFO kuyruguna göre daha az bekleyen müþteri");
+				System.out.println(temp.sira+". müþteri ve beklediði süre "+bekleme_suresi_oncelik[temp.sira-1]);
+			}
+			
+			toplam_sure+=sure;
+			
+		}
+		System.out.println("Öncelikli bekleme süresi:"+(float)toplam_sure/boyut);
+		gise_onc_ort=(float)toplam_sure/boyut;
+	}
+
+	public float onc_ort(){
+		
+		return gise_onc_ort;
+		}
+   public void ortalamalarý_yaz(){
+	   System.out.println("Tek fifo kuyruðu için ortalama süre:"+ort_sure);
+	   System.out.println("Tek öncelik kuyruðu için ortalama süre:"+onc_ort_sure);
+	   
+   }
+
+
 }
-
-
-
